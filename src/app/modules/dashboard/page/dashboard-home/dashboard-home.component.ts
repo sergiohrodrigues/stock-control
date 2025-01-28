1db7fc98508c10026f8ec8c44d6ba12a6d483900
Sergio <sergiohrodriguess@gmail.com>
2025-01-28 10:38:22 -0300

    implement graphic chart.js

--- src/app/modules/dashboard/page/dashboard-home/dashboard-home.component.ts
+++ src/app/modules/dashboard/page/dashboard-home/dashboard-home.component.ts
@@ -53,7 +53,59 @@ export class DashboardHomeComponent implements OnInit, OnDestroy {
   }
 
   setProductsChartConfig(): void{
+    if(this.productsList.length > 0){
+      const documentStyle = getComputedStyle(document.documentElement);
+      const textColor = documentStyle.getPropertyValue('--text-color');
+      const textColorSecondary = documentStyle.getPropertyValue('--text-color-secondary')
+      const surfaceBorder = documentStyle.getPropertyValue('--surface-border');
 
+      this.productsChartDatas = {
+        labels: this.productsList.map((element) => element.name),
+        datasets: [
+          {
+            label: 'Quantidade',
+            backgroundColor: documentStyle.getPropertyValue('--indigo-400'),
+            borderColor: documentStyle.getPropertyValue('--indigo-400'),
+            hoverBackgroundColor: documentStyle.getPropertyValue('--indigo-500'),
+            data: this.productsList.map((element) => element?.amount),
+          },
+        ],
+      };
+
+      this.productsChartOptions = {
+        maintainAspectRatio: false,
+        aspectRatio: 0.8,
+        plugins: {
+          legend: {
+            labels: {
+              color: textColor
+            }
+          }
+        },
+
+        scales: {
+          x: {
+            ticks: {
+              color: textColorSecondary,
+              font: {
+                weight: 500
+              }
+            },
+            grid: {
+              color: surfaceBorder
+            }
+          },
+          y: {
+            ticks:{
+              color: textColorSecondary
+            },
+            grid: {
+              color: surfaceBorder
+            }
+          }
+        },
+      }
+    }
   }
 
   ngOnDestroy(): void {
