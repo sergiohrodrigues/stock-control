611f5909d796e02b6deb44bdd2ae8c6466fd0218
Sérgio Henrique Rodrigues <sergio.rodrigues@ravex.local>
2025-01-28 09:32:53 -0300

    first commit

---
+++ src/app/modules/dashboard/page/dashboard-home/dashboard-home.component.ts
@@ -0,0 +1,63 @@
+import { ProductsDataTransferService } from './../../../../shared/services/products/products-data-transfer.service';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ProductsService } from 'src/app/services/products/products.service';
+import {  } from 'primeng/message'
+import { MessageService } from 'primeng/api';
+import { GetAllProductsResponse } from 'src/app/models/interfaces/products/response/GetAllProductsResponse';
+import { Subject, takeUntil } from 'rxjs';
+import { ChartData, ChartOptions } from 'chart.js';
+
+@Component({
+  selector: 'app-dashboard-home',
+  templateUrl: './dashboard-home.component.html',
+  styleUrls: []
+})
+export class DashboardHomeComponent implements OnInit, OnDestroy {
+  private destroy$ = new Subject<void>();
+  public productsList: Array<GetAllProductsResponse> = [];
+
+  public productsChartDatas!: ChartData;
+  public productsChartOptions!: ChartOptions;
+
+  constructor(
+    private productsService: ProductsService,
+    private messageService: MessageService,
+    private productsDtService: ProductsDataTransferService
+  ){}
+
+  ngOnInit(): void {
+    this.getProductsDatas()
+  }
+
+  getProductsDatas(): void {
+    this.productsService.getAllProduts()
+      .pipe(takeUntil(this.destroy$))
+      .subscribe({
+      next: (response) => {
+        if(response.length > 0){
+          this.productsList = response;
+          this.productsDtService.setProductsDatas(this.productsList);
+          this.setProductsChartConfig();
+        }
+      },
+      error: (err) => {
+        console.log(err)
+        this.messageService.add({
+          severity: 'error',
+          summary: 'Erro',
+          detail: 'Erro ao buscar produtos',
+          life: 2500
+        })
+      }
+    })
+  }
+
+  setProductsChartConfig(): void{
+
+  }
+
+  ngOnDestroy(): void {
+   this.destroy$.next();
+   this.destroy$.complete();
+  }
+}
