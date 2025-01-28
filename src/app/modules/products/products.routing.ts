6593ed7840bd80cc32be988a5eceaadb1d9fb0a5
Sergio <sergiohrodriguess@gmail.com>
2025-01-28 11:10:07 -0300

    gerando modules, routing, and products view

---
+++ src/app/modules/products/products.routing.ts
@@ -0,0 +1,9 @@
+import { Routes } from "@angular/router";
+import { ProductsHomeComponent } from "./products-home/products-home.component";
+
+export const PRODUCTS_ROUTES: Routes = [
+  {
+    path: '',
+    component: ProductsHomeComponent
+  }
+]
